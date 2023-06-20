d7c30e48cdf5796d02f3e40457ca9fcee13e9135
Priyabrata Maji <priyabratamaji139@gmail.com>
2023-06-20 17:07:12 +0530

    Added Basic Node project template

---
+++ src/routes/v1/airplane-routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const { AirplaneController } = require('../../controllers');
+const { AirplaneMiddlewares } = require('../../middlewares');
+
+const router = express.Router();
+
+// /api/v1/airplanes POST
+router.post('/', 
+        AirplaneMiddlewares.validateCreateRequest,
+        AirplaneController.createAirplane);
+
+// /api/v1/airplanes GET
+router.get('/', 
+        AirplaneController.getAirplanes);
+
+// /api/v1/airplanes/:id GET
+router.get('/:id', 
+        AirplaneController.getAirplane);
+
+module.exports = router;
